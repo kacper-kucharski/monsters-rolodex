5a99f7e06e3d31a2ec968ecbf3c52e35b85c51b0
Kacper Kucharski <kacpikuch1@icloud.com>
2020-09-28 16:07:14 +0200

    Updates

---
+++ precache-manifest.e0d9f552fba3ebc2bbe432b650746f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e66e54cf1b602724161bc735f60efa9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "237c279f345f4925025c",
+    "url": "/monsters-rolodex/static/css/main.48cafb80.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237c279f345f4925025c",
+    "url": "/monsters-rolodex/static/js/main.82862938.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
